71026cce4fc4732544a147b8910c615bbdcb46c3
jorgeality <jorgeality29@gmail.com>
2018-01-04 01:38:05 -0500

    esto no sirve

---
+++ src/client/src/app/Task.ts
@@ -0,0 +1,9 @@
+export class Task{
+    _id?: string; // el ? significa que esta variable es opcional
+    title: string;
+    isDone: boolean;
+    n?: number;
+
+}
+
+//y esta clase debe ser exportada para ser usada en todo el proyecto
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 
+//importando servicios, de igual manera los debemos incluir en los providers
+import {TasksService} from './services/tasks.service';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot() // <-- Esta línea también la añadimos
   ],
-  providers: [],
+  providers: [TasksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
